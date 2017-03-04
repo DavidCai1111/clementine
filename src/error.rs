3b1624ea0c7b56bcae1660e2c510d0ea64039012
DavidCai <davidcai1993@yahoo.com>
2017-03-04 16:46:42 +0800

    feat: implement basic error structure

---
+++ src/error.rs
@@ -0,0 +1,43 @@
+use std::error;
+use std::fmt;
+use std::result;
+
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    TransactionNotWritable,
+    ItemNotFound,
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub kind: ErrorKind,
+}
+
+impl Error {
+    pub fn message(&self) -> &str {
+        match self.kind {
+            ErrorKind::TransactionNotWritable => "transaction is not writable",
+            ErrorKind::ItemNotFound => "item not found",
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[clementine error]: {:?}", self.message())
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        self.message()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error { kind: kind }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub use self::error::*;
+
+mod error;
