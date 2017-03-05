228415dfeb01238bed6ed4da18fb07684be00204
DavidCai <davidcai1993@yahoo.com>
2017-03-05 11:35:40 +0800

    feat: add basic transaction type

--- src/database.rs
+++ src/database.rs
@@ -1,20 +1,26 @@
 use std::collections::BTreeMap;
 use std::sync::RwLock;
+use std::cmp::Ord;
 use error::{Error, ErrorKind, Result};
+use transaction::Transaction;
 
 #[derive(Debug)]
-pub struct Database {
-    store: BTreeMap<String, String>,
-    mutex: RwLock<()>,
+pub struct Database<K, V>
+    where K: Ord
+{
+    store: BTreeMap<K, V>,
     closed: bool,
+    txn_mut: RwLock<Transaction>,
 }
 
-impl Database {
-    pub fn new() -> Result<Database> {
+impl<'a, K, V> Database<K, V>
+    where K: Ord
+{
+    pub fn new() -> Result<Database<K, V>> {
         Ok(Database {
             store: BTreeMap::new(),
-            mutex: RwLock::new(()),
             closed: false,
+            txn_mut: RwLock::new(Transaction {}),
         })
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub use self::database::Database;
+pub use self::transaction::Transaction;
 pub use self::error::*;
 
 mod database;
 mod error;
+mod transaction;
