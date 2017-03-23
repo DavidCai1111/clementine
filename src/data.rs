04c6de05b4dcc3c99da2b9cb2d2d4fa63281bb07
DavidCai <davidcai1993@yahoo.com>
2017-03-23 17:34:41 +0800

    feat: add string && int serialization

--- src/data.rs
+++ src/data.rs
@@ -12,30 +12,54 @@ pub trait Serializable
     fn into(Self) -> String;
 }
 
+// TODO: JSON support: https://github.com/serde-rs/json .
 #[derive(Debug)]
 pub enum Data {
     String(String),
-    Int(i64), 
-    // JSON(String),
+    Int(i64),
+}
+
+impl Data {
+    fn from_string(s: String) -> Result<Self> {
+        Ok(Data::String(s[1..s.len() - 2].to_string()))
+    }
+
+    fn from_int(s: String) -> Result<Self> {
+        match s[1..s.len() - 2].parse::<i64>() {
+            Ok(int) => Ok(Data::Int(int)),
+            Err(_) => Err(Error::new(ErrorKind::InvalidSerializedString)),
+        }
+    }
+
+    fn serialize_string(s: String) -> String {
+        STRING_PREFIX.to_string() + &s + CRLF
+    }
+
+    fn serialize_int(i: i64) -> String {
+        INT_PREFIX.to_string() + &i.to_string() + CRLF
+    }
 }
 
 impl Serializable for Data {
     fn into(data: Self) -> String {
         match data {
-            Data::String(string) => STRING_PREFIX.to_string() + &string + CRLF,
-            Data::Int(int) => INT_PREFIX.to_string() + &int.to_string() + CRLF,
+            Data::String(string) => Self::serialize_string(string),
+            Data::Int(int) => Self::serialize_int(int),
         }
     }
 
-    fn try_from(s: String) -> Result<Self> {
-        // TODO: convert the string.
-        if s.starts_with(STRING_PREFIX) {
-            return Ok(Data::Int(1));
+    fn try_from(string: String) -> Result<Self> {
+        if string.len() <= 2 || string.ends_with(CRLF) {
+            return Err(Error::new(ErrorKind::InvalidSerializedString));
         }
-        if s.starts_with(INT_PREFIX) {
-            return Ok(Data::Int(1));
+
+        if string.starts_with(STRING_PREFIX) {
+            Ok(Self::from_string(string)?)
+        } else if string.starts_with(INT_PREFIX) {
+            Ok(Self::from_int(string)?)
+        } else {
+            Err(Error::new(ErrorKind::InvalidSerializedString))
         }
-        Err(Error::new(ErrorKind::InvalidSerializedString))
     }
 }
 
