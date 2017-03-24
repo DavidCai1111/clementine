9cac8f043004081228f935b520539932f1875954
DavidCai <davidcai1993@yahoo.com>
2017-03-24 17:12:57 +0800

    test: add more data tests

--- src/data.rs
+++ src/data.rs
@@ -10,7 +10,7 @@ pub trait Serializable: Clone
     where Self: Sized
 {
     fn try_from(String) -> Result<Self>;
-    fn into(Self) -> String;
+    fn into_string(self) -> String;
 }
 
 // TODO: JSON support: https://github.com/serde-rs/json .
@@ -34,8 +34,8 @@ impl<S: Into<String> + Clone> Data<S> {
         }
     }
 
-    fn serialize_string(s: String) -> String {
-        STRING_PREFIX.to_string() + &s + CRLF
+    fn serialize_string(s: S) -> String {
+        STRING_PREFIX.to_string() + &s.into() + CRLF
     }
 
     fn serialize_int(i: i64) -> String {
@@ -44,15 +44,15 @@ impl<S: Into<String> + Clone> Data<S> {
 }
 
 impl Serializable for Data<String> {
-    fn into(data: Self) -> String {
-        match data {
-            Data::String(string) => Self::serialize_string(string.into()),
+    fn into_string(self) -> String {
+        match self {
+            Data::String(string) => Self::serialize_string(string),
             Data::Int(int) => Self::serialize_int(int),
         }
     }
 
     fn try_from(string: String) -> Result<Data<String>> {
-        if string.len() <= 2 || string.ends_with(CRLF) {
+        if string.len() < 2 || !string.ends_with(CRLF) {
             return Err(Error::new(ErrorKind::InvalidSerializedString));
         }
 
@@ -93,12 +93,53 @@ mod tests {
 
     #[test]
     fn test_from_string() {
-        assert_eq!(Data::String("test_from_string".to_string()),
-                   Data::from_string("+test_from_string\r\n").unwrap());
+        assert_eq!(Data::String("test_\r\nfrom_string".to_string()),
+                   Data::from_string("+test_\r\nfrom_string\r\n").unwrap());
     }
 
     #[test]
     fn test_from_int() {
         assert_eq!(Data::Int(22), Data::from_int(":22\r\n").unwrap());
+    }
+
+    #[test]
+    fn test_from_invalid_int() {
+        assert!(Data::from_int(":22invalid888\r\n").is_err());
+    }
+
+    #[test]
+    fn test_serialize_string() {
+        assert_eq!("+test\r\n_serialize\r\n_string\r\n",
+                   Data::serialize_string("test\r\n_serialize\r\n_string"));
+    }
+
+    #[test]
+    fn test_serialize_int() {
+        assert_eq!(":666\r\n", Data::<String>::serialize_int(666));
+    }
+
+    #[test]
+    fn test_serializble_into() {
+        assert_eq!("+666\r\n", Data::String("666".to_string()).into_string());
+        assert_eq!(":666\r\n", Data::Int(666).into_string());
+    }
+
+    #[test]
+    fn test_try_from_invalid() {
+        assert!(Data::try_from("".to_string()).is_err());
+        assert!(Data::try_from("\r\n".to_string()).is_err());
+        assert!(Data::try_from("11111".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_try_from_string() {
+        assert_eq!(Data::String("666".to_string()),
+                   Data::try_from("+666\r\n".to_string()).unwrap());
+    }
+
+    #[test]
+    fn test_try_from_int() {
+        assert_eq!(Data::Int(666),
+                   Data::try_from(":666\r\n".to_string()).unwrap());
     }
 }
